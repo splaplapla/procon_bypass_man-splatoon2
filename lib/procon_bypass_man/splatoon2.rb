7fb3497e34b511e76702c748e494f3bfe3a6b566
jiikko <n905i.1214@gmail.com>
2021-06-19 14:15:38 +0900

    first

---
+++ lib/procon_bypass_man/splatoon2.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "splatoon2/version"
+
+module ProconBypassMan
+  module Splatoon2
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
